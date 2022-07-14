90bc5ea3768828e3d4376af7afddc614264a2543
jonghwan3 <pjhan93@hanyang.ac.kr>
2022-07-14 23:07:42 +0900

    iOS version에 따른 datePickerStyle 변경, datePicker 선택한 날짜로부터 d-Day 구하기

--- SeSACWeek2/AnniversaryCalculatorViewController.swift
+++ SeSACWeek2/AnniversaryCalculatorViewController.swift
@@ -8,13 +8,44 @@
 import UIKit
 
 class AnniversaryCalculatorViewController: UIViewController {
-
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet var imageViewCollection: [UIImageView]!
+    @IBOutlet var dateLabelList: [UILabel]!
     override func viewDidLoad() {
         super.viewDidLoad()
         designDiaryImageView(imageCollection: imageViewCollection)
-        dateFormatStyle()
-        numberFormatStyle()
+        
+        //MARK: - DatePicker 버전에 따른 대응
+        if #available(iOS 14, *) {
+            datePicker.preferredDatePickerStyle = .inline
+        } else {
+            print("버전 14 미만?")
+            //preferredDatePickerStyle' is only available in iOS 13.4 or newer 에러로 13.0 버전에서는 해당 코드 실행 안되어, 스토리보드에 wheels로 설정
+            //datePicker.preferredDatePickerStyle = .wheels
+        }
+        //dateFormatStyle()
+        //numberFormatStyle()
+        //알아보기 쉬운 날짜 + 시간대 (yyyy MM dd hh:mm:ss)
+//        let format = DateFormatter()
+//        format.dateFormat = "M월 d일, yy년"
+//
+//        let result = format.string(from: Date())
+//        print(result, Date())
+//
+//        let word = "3월 2일, 19년"
+//        let dateResult = format.date(from: word)
+//        print(dateResult!)
+    }
+    //MARK: - DatePicker 선택 시, 해당 날짜로부터 D-day 계산
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        let format = DateFormatter()
+        format.dateFormat = "yyyy년\nMM월 dd일"
+        let secPDay: Double = 86400 // 60s/1m * 60m/1h * 24h/1d
+        var dDay: Double = 100
+        for label in dateLabelList {
+            label.text = format.string(from: sender.date.addingTimeInterval(secPDay * dDay))
+            dDay += 100
+        }
     }
     
     func designDiaryImageView(imageCollection: [UIImageView]) {
@@ -23,7 +54,7 @@ class AnniversaryCalculatorViewController: UIViewController {
             imageView.clipsToBounds = true
         }
     }
-    
+    //MARK: - Practice
     func dateFormatStyle() {
         let value = Date()
         
